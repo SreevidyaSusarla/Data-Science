026f30b61b409a03b7fd5a8c7d756c1d3ecdf132
Sreevidya Susarla <susarla.sreevidya94@gmail.com>
2016-12-05 22:20:59 -0700

    Add files via upload

---
+++ Cluster.java
@@ -0,0 +1,75 @@
+ import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+ 
+import weka.clusterers.SimpleKMeans;
+import weka.core.Instances;
+ 
+public class Cluster {
+ 
+	public static BufferedReader readDataFile(String filename) {
+		BufferedReader inputReader = null;
+ 
+		try {
+			inputReader = new BufferedReader(new FileReader(filename));
+		} catch (FileNotFoundException ex) {
+			System.err.println("File not found: " + filename);
+		}
+ 
+		return inputReader;
+	}
+ 
+	public static void main(String[] args) throws Exception {
+		SimpleKMeans kmeans = new SimpleKMeans();
+ 
+		kmeans.setSeed(10);
+ 
+		//important parameter to set: preserver order, number of cluster.
+		kmeans.setPreserveInstancesOrder(true);
+		kmeans.setNumClusters(5);
+ 
+		//BufferedReader datafile = readDataFile("C:/Users/susar/Desktop/Teams.arff"); 
+		BufferedReader datafile = readDataFile("C:/Users/susar/Desktop/storm.arff");
+		Instances data = new Instances(datafile);
+ 
+ 
+		kmeans.buildClusterer(data);
+ 
+		// This array returns the cluster number (starting with 0) for each instance
+		// The array has as many elements as the number of instances
+		int[] assignments = kmeans.getAssignments();
+ 
+		int i=0;
+		int cust0=0,cust1=0,cust2=0,cust3=0,cust4=0;
+		for(int clusterNum : assignments) {
+		    //System.out.printf("Instance %d -> Cluster %d \n", i, clusterNum);
+		    if(clusterNum==0)
+		    {
+		    	cust0++;
+		    }
+		    else if(clusterNum==1)
+		    {
+		    	cust1++;
+		    }
+		    else if(clusterNum==2)
+		    {
+		    	cust2++;
+		    }
+		    else if(clusterNum==3)
+		    {
+		    	cust3++;
+		    }
+		    else 
+		    {
+		    	cust4++;
+		    }
+		   // i++;
+		}
+		System.out.println("Cluster 0:"+cust0);
+		System.out.println("Cluster 1:"+cust1);
+		System.out.println("Cluster 2:"+cust2);
+		System.out.println("Cluster 3:"+cust3);
+		System.out.println("Cluster 4:"+cust4);
+		
+	}
+}
